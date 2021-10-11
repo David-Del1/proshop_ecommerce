682581750b42f805043508692c6c0cda48619677
David Delgadillo <daviddel.731@gmail.com>
2021-10-11 16:48:24 -0700

    Installed react-bootstrap npm package and added bootstrap css file as well as font awesome icon cdn link inside of public html. achieved base layout and styling of header

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app/App";
 
+import './index.css';
+import './bootstrap.min.css';
+
 ReactDOM.render(
   <App />,
   document.getElementById("root")
