1a8b3587b2f12ebcea9290756e0b759c6f9b1584
David Delgadillo <daviddel.731@gmail.com>
2021-10-13 19:43:16 -0700

    Fixed the 'product.map is not a function' bug. The problem was in my constants.js file from not renaming constants from PRODUCT_LIST to PRODUCT_DETAIL

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -28,8 +28,14 @@ const HomeScreen = () => {
       ) 
       : (
         <Row>
-          {products?.map(product => (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+          {products.map(product => (
+            <Col
+              className="align-items-stretch d-flex"
+              key={product._id} 
+              sm={12} 
+              md={6} 
+              lg={4} 
+              xl={3}>
               <Product product={product} />
             </Col>
           ))}
