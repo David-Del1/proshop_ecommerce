5b34b0fe08632d21938d6d6c1542417aa781a79e
David Delgadillo <daviddel.731@gmail.com>
2021-10-14 22:26:43 -0700

    Logic incorporated for CartScreen component

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -5,7 +5,7 @@ import Footer from '../components/footer/Footer';
 import Header from '../components/header/Header';
 import HomeScreen from '../screens/HomeScreen';
 import ProductScreen from '../screens/ProductScreen';
-import CartScreen from '../screens/CartScreen';
+import CartScreen from '../screens/CartScreen.jsx';
 
 function App() {
   return (
