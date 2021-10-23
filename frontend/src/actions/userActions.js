138a808b7ccc139dd69bb4a322ab1d54cfe42260
David Delgadillo <daviddel.731@gmail.com>
2021-10-22 17:30:13 -0700

    Updated Header component to display name of user upon successful login. Added dropdown menu that contains two links. Also fixed a bug where userInfo was pointing to cartItems instead of userInfo in local storage

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,20 +1,17 @@
 import axios from 'axios';
-import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAILURE } from '../constants/userConstants';
+import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAILURE, USER_LOGOUT } from '../constants/userConstants';
 
 export const login = (email, password) => async (dispatch) => {
   try {
     dispatch({
       type: USER_LOGIN_REQUEST
     });
-
     const config = {
       headers: {
         'Content-Type': 'application/json'
       }
     }
-
-    const { data }= await axios.post('/api/users/login', { email, password }, config)
-
+    const { data } = await axios.post('/api/users/login', { email, password }, config)
     dispatch({
       type: USER_LOGIN_SUCCESS,
       payload: data
@@ -29,4 +26,9 @@ export const login = (email, password) => async (dispatch) => {
         : error.message
     });
   }
+}
+
+export const logout = () => (dispatch) => {
+  localStorage.removeItem('userInfo');
+  dispatch({ type: USER_LOGOUT });
 }
