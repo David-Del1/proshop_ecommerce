3f8d4572e238a0c9eb4f0c980d20d2efe5d63870
David Delgadillo <daviddel.731@gmail.com>
2021-12-02 19:54:10 -0800

    Created new User Update screen and relevant route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,12 +3,15 @@ const router = express.Router();
 import { authUser, deleteUser, getUserById, getUserProfile, getUsers, registerUser, updateUser, updateUserProfile } from '../controllers/user.js';
 import { adminMiddleware, protect } from '../middleware/authMiddleware.js';
 
-router.route('/').post(registerUser).get(protect, adminMiddleware, getUsers)
+router.route('/')
+  .post(registerUser)
+  .get(protect, adminMiddleware, getUsers)
 router.post('/login', authUser);
 router.route('/profile')
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile);
-router.route('/:id').delete(protect, adminMiddleware, deleteUser)
+router.route('/:id')
+  .delete(protect, adminMiddleware, deleteUser)
   .get(protect, adminMiddleware, getUserById)
   .put(protect, adminMiddleware, updateUser)
 
