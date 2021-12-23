482aec6771c86bde4dc8f399e7315399f92760c8
David Delgadillo <daviddel.731@gmail.com>
2021-12-22 23:52:07 -0800

    Added product update route and controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { deleteProduct, getProductById, getProducts } from '../controllers/product.js';
+import { createProduct, deleteProduct, getProductById, getProducts, updateProduct } from '../controllers/product.js';
 const router = express.Router();
 import { protect, adminMiddleware } from '../middleware/authMiddleware.js';
 
-router.route('/').get(getProducts);
+router.route('/').get(getProducts)
+  .post(protect, adminMiddleware, createProduct)
 router.route('/:id').get(getProductById)
   .delete(protect, adminMiddleware, deleteProduct)
+  .put(protect, adminMiddleware, updateProduct)
 
 export default router;
